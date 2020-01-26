ff648111efad4360470959d781acc9291fb0a292
Paul Beltran <paulbeltran123456@gmail.com>
2020-01-25 20:48:55 -0800

    My first commit

---
+++ 11932902_exer3.py
@@ -0,0 +1,10 @@
+print('#' * 50)
+print('# Author: Paul Joshua C. Beltran')
+print('# Date: January 25, 2020')
+print('# Course: LBYCPA1\n\n')
+print('# Program Description\n')
+print('''LBYCPA1 is a program centered more on the programming language Python.
+This course encompasses its syntax, logic formulation, application design workflow and object-oriented programming.
+Lastly, some of the topics included are boolean & module, sequences & iteration, string & formatting, file I/O handling
+and many more.''')
+print('#' * 50)
